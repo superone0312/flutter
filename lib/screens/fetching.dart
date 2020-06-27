1331485430775ffa0f55eb17358abc8fa8c12718
Charlie W <charliew.@cw2015laptop.local>
2020-06-27 11:16:29 +0800

    tutorial32Spinner

--- lib/screens/fetching.dart
+++ lib/screens/fetching.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:word_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Fetching extends StatefulWidget {
   @override
@@ -12,11 +13,12 @@ class _FetchingState extends State<Fetching> {
   void setupWorldTime() async {
   WorldTime instance = WorldTime(place: 'Shanghai', flag: 'China.png', url: 'Asia/Shanghai');
   await instance.getWatch();
+  
   Navigator.pushReplacementNamed(context, '/index', arguments: {
     'place': instance.place,
     'flag': instance.flag,
-    'clock': instance.clock,
-  });
+    'clock': instance.clock ,
+    });
   }
 
   @override
@@ -28,8 +30,14 @@ class _FetchingState extends State<Fetching> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      SafeArea(child: Text('Fetching...')),
+      backgroundColor: Colors.blueAccent,
+      body:
+      Center(
+        child: SpinKitRing(
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
     );
   }
 }
